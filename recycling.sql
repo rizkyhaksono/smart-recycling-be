b40f0e712c1822cdd716e6670149a6def04c89c3
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-11-23 14:06:39 +0700

    feat(backup): mysql backup

--- recycling.sql
+++ recycling.sql
@@ -1,145 +1,54 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: db
--- Waktu pembuatan: 10 Nov 2023 pada 11.29
--- Versi server: 8.1.0
--- Versi PHP: 8.2.10
+CREATE TABLE User (
+  id VARCHAR(36) PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
+  password VARCHAR(255),
+  role ENUM('ADMIN', 'USER'),
+  points INT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE Item (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  points VARCHAR(255),
+  total INT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE Event (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  description TEXT,
+  path_image VARCHAR(255),
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  user_id VARCHAR(36),
+  FOREIGN KEY (user_id) REFERENCES User(id)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE ExchangeTransaction (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  items_id INT,
+  user_id VARCHAR(36),
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (items_id) REFERENCES Item(id),
+  FOREIGN KEY (user_id) REFERENCES User(id)
+);
 
---
--- Database: `recycling`
---
+CREATE TABLE RefreshToken (
+  user_id VARCHAR(36) PRIMARY KEY,
+  token VARCHAR(255) UNIQUE,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES User(id)
+);
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `events`
---
-
-CREATE TABLE `events` (
-  `id` int NOT NULL,
-  `title` varchar(2000) NOT NULL,
-  `description` text NOT NULL,
-  `created_at` timestamp NOT NULL,
-  `updated_at` timestamp NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data untuk tabel `events`
---
-
-INSERT INTO `events` (`id`, `title`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Sampah di Kota Malang menumpuk', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book', '2023-11-10 11:29:23', '2023-11-10 11:29:23'),
-(2, 'Sampah nya banyak banget di Malang', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book', '2023-11-10 11:29:36', '2023-11-10 11:29:36');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `refresh_tokens`
---
-
-CREATE TABLE `refresh_tokens` (
-  `user_id` int NOT NULL,
-  `token` varchar(35) COLLATE utf8mb4_general_ci NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `refresh_tokens`
---
-
-INSERT INTO `refresh_tokens` (`user_id`, `token`, `created_at`) VALUES
-(1, 'b3e1ff8a1b7b36d8fdf967e89076f3c2', '2023-11-10 03:40:22'),
-(1, 'fe7d10d91af6a6949f8e48acb982cc96', '2023-11-10 10:34:51'),
-(1, '858a10f498288ed9228cb24fe3edf22f', '2023-11-10 10:48:00'),
-(2, '85de2f4cbad35534f13a42cbaaeee57f', '2023-11-10 11:09:16');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `users`
---
-
-CREATE TABLE `users` (
-  `id` int NOT NULL,
-  `name` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `points` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`, `points`) VALUES
-(1, 'rizky', 'user@email.com', '$2b$10$fK1uMVu6wVVRqld/NtRls.JzCIm.YfyvHltnAQmJQEkt7WRxZumK2', '2023-11-10 03:38:20', '2023-11-10 03:38:20', 1000),
-(2, 'test 1', 'test1@email.com', '$2b$10$NHgBMZ34Po1QeXmQdpASpeVqtSLeke39.LFMpSMGgvhHAmpN5sHGG', '2023-11-10 11:08:38', '2023-11-10 11:08:38', NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `events`
---
-ALTER TABLE `events`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `refresh_tokens`
---
-ALTER TABLE `refresh_tokens`
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `token` (`token`);
-
---
--- Indeks untuk tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `events`
---
-ALTER TABLE `events`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `refresh_tokens`
---
-ALTER TABLE `refresh_tokens`
-  ADD CONSTRAINT `refresh_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE Report (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255),
+  subject VARCHAR(255),
+  location VARCHAR(255),
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  user_id VARCHAR(36),
+  FOREIGN KEY (user_id) REFERENCES User(id)
+);
